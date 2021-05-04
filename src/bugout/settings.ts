ff2d385d13931122cc4a93cf51f74f368c97564d
kompotkot <zenitsws@gmail.com>
2021-05-04 18:41:14 +0000

    Fixed naming in properties

--- src/bugout/settings.ts
+++ src/bugout/settings.ts
@@ -3,4 +3,4 @@ import { workspace } from "vscode"
 const configuration = workspace.getConfiguration()
 export const bugoutSpireUrl: string | undefined = configuration.get("Bugout.Api.endpoint")
 export const bugoutAccessToken: string | undefined = configuration.get("Bugout.AccessToken")
-export const bugoutHumbugJournalId: string | undefined = configuration.get("Bugout.Journal")
+export const bugoutHumbugJournalId: string | undefined = configuration.get("Bugout.CrashReportsJournal")
